44d1064a2bf9b2e063914578dadc020879cf278a
Nwankwo Samuel <nwankwosami@gmail.com>
2020-01-28 00:55:28 +0100

    added css files

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2018 Online Library Management System |<a href="https://facebook.com/nwankwo.samuel" target="_blank"  > Designed by : Nwankwo Samuel</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
